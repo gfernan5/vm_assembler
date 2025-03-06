fa7d3c3be2a29a96cb24fc5add4d13b9e2cf0259
David Emmanuel Long <dlong37@hydra18.eecs.utk.edu>
2025-03-06 16:44:17 -0500

    added dup function to file

---
+++ Instructions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+
+public class Dup : IInstruction {
+    private readonly int _offset;
+    public Dup(int offset) {
+        _offset = offset & ~3;
+    }
+    public int Encode() {
+        return (0b1100 << 28) | _offset;
+    }
+}
