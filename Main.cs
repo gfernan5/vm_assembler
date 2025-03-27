f771b4a7f36216597fb6f48e01030813cc06a3bc
David Emmanuel Long <dlong37@hydra18.eecs.utk.edu>
2025-03-26 21:56:07 -0400

    trimmed whitespace/stored labels

--- Main.cs
+++ Main.cs
@@ -8,12 +8,17 @@
 
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 class Assembler
 {
     public static void Main(string[] args)
     {
         int programCounter = 0;
+        int lineCounter = 0;
+        Dictionary<string, int> labelMap = new Dictionary<string, int>();
+        List<string> _instructionList = new List<string>();
+        List<IInstruction> instructionList = new List<IInstruction>();
 
         // check CMD line arguments
         if (args.Length != 2) {
@@ -38,6 +43,8 @@ class Assembler
         while ((line = sr.ReadLine()) != null) {
             // clear all comments and trim whitespace
             line = line.Split('#')[0].Trim();
+            string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            line = string.Join(" ", parts);
 
             // skip any empty lines
             if (string.IsNullOrEmpty(line)) {
@@ -47,13 +54,15 @@ class Assembler
             // process labels
             if (line.EndsWith(':')) {
                 string label = line.Substring(0, line.Length - 1);
-                
+                labelMap[label] = programCounter;
                 continue;
             }
 
+            _instructionList.Add(line);
             Console.WriteLine(line);
 
             programCounter += 4;
+            lineCounter++;
         }
     }
 }
