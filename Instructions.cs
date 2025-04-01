e0c6fb11a00f8ec5eadd4ef9adea09d8b37470f6
EthBeats <ethbeats3@gmail.com>
2025-03-31 22:52:21 -0400

    updated Main

--- Instructions.cs
+++ Instructions.cs
@@ -33,7 +33,7 @@ public class Swap : IInstruction {
 }
 
 // NOP Instruction
-public class NOP : IInstruction {
+public class Nop : IInstruction {
     public int Encode() {
         return (0b0010 << 24);
     }
@@ -75,5 +75,273 @@ public class Dup : IInstruction {
 public class Dump : IInstruction {
     public int Encode() {
         return (0b1110 << 28);
+    }
+}
+
+/* Binary Arithmetic Instructions */
+
+// add instruction
+public class Add : IInstruction
+{
+    public int Encode()
+    {
+        int opcode = 0b0010 << 28;
+        int operation = 0b0000 << 24;
+        return opcode | operation;
+    }
+}
+
+// sub instruction
+public class Sub : IInstruction
+{
+    public int Encode()
+    {
+        int opcode = 0b0010 << 28;
+        int operation = 0b0001 << 24;
+        return opcode | operation;
+    }
+}
+
+// mul instruction
+public class Mul : IInstruction
+{
+    public int Encode()
+    {
+        int opcode = 0b0010 << 28;
+        int operation = 0b0010 << 24;
+        return opcode | operation;
+    }
+}
+
+// div instruction
+public class Div : IInstruction
+{
+    public int Encode()
+    {
+        int opcode = 0b0010 << 28;
+        int operation = 0b0011 << 24;
+        return opcode | operation;
+    }
+}
+
+// rem instruction
+public class Rem : IInstruction
+{
+    public int Encode()
+    {
+        int opcode = 0b0010 << 28;
+        int operation = 0b0100 << 24;
+        return opcode | operation;
+    }
+}
+
+// and instruction
+public class And : IInstruction
+{
+    public int Encode()
+    {
+        int opcode = 0b0010 << 28;
+        int operation = 0b0101 << 24;
+        return opcode | operation;
+    }
+}
+
+// or instruction
+public class Or : IInstruction
+{
+    public int Encode()
+    {
+        int opcode = 0b0010 << 28;
+        int operation = 0b0110 << 24;
+        return opcode | operation;
+    }
+}
+
+// xor instruction
+public class Xor : IInstruction
+{
+    public int Encode()
+    {
+        int opcode = 0b0010 << 28;
+        int operation = 0b0111 << 24;
+        return opcode | operation;
+    }
+}
+
+// lsl instruction
+public class Lsl : IInstruction
+{
+    public int Encode()
+    {
+        int opcode = 0b0010 << 28;
+        int operation = 0b1000 << 24;
+        return opcode | operation;
+    }
+}
+
+// lsr instruction
+public class Lsr : IInstruction
+{
+    public int Encode()
+    {
+        int opcode = 0b0010 << 28;
+        int operation = 0b1001 << 24;
+        return opcode | operation;
+    }
+}
+
+// asr instruction
+public class Asr : IInstruction
+{
+    public int Encode()
+    {
+        int opcode = 0b0010 << 28;
+        int operation = 0b1011 << 24;
+        return opcode | operation;
+    }
+}
+
+// Debug
+public class Debug : IInstruction {
+    private readonly int _value;
+
+    public Debug(int value = 0) {
+        _value = value & 0x00FF_FFFF;
+    }
+
+    public int Encode() {
+        return (0b0000 << 28) | (0b1111 << 24) | _value;
+    }
+}
+
+// Pop - Opcode 1
+public class Pop : IInstruction {
+    private readonly int _offset;
+
+    public Pop(int offset = 4) {
+        _offset = offset >> 2;
+    }
+
+    public int Encode() {
+        return (0b0001 << 28) | (_offset << 2);
+    }
+}
+
+// Neg - Opcode 3
+public class Neg : IInstruction {
+    public int Encode() {
+        return (0b0011 << 28) | (0b0000 << 24);
+    }
+}
+
+// Not - Opcode 3
+public class Not : IInstruction {
+    public int Encode() {
+        return (0b0011 << 28) | (0b0001 << 24);
+    }
+}
+
+// String print - Opcode 4
+public class Stprint : IInstruction {
+    private readonly int _offset;
+
+    public Stprint(int offset = 0) {
+        _offset = offset;
+    }
+
+    public int Encode() {
+        return (0b0100 << 28) | (_offset & 0x0FFFFFFF);
+    }
+}
+
+// Print - Opcode 13
+public class Print : IInstruction {
+    private readonly int _fmt;
+    private readonly int _offset;
+
+    public Print(int fmt = 0, int offset = 0) {
+        _offset = offset & 0x0FFFFFFC;
+        if(fmt == 1) {
+            _fmt = 0b0001;
+        }
+        else if(fmt == 2) {
+            _fmt = 0b0010;
+        }
+        else if(fmt == 3) {
+            _fmt = 0b0011;
+        }
+        else {
+            _fmt = 0b0000;
+        }
+    }
+
+    public int Encode() {
+        return (0b1101 << 28) | _offset | _fmt;
+    }
+}
+
+// Unconditional Goto
+// Extract PC from label and put into PC offset of instruction
+public class Goto : IInstruction {
+    private readonly int _label;
+    public Goto(int label) {
+        _label = label;
+    }
+    public int Encode() {
+        return (0b0111 << 28) | _label;
+    }
+}
+
+// Call
+// Extract PC, clear out bottom bits, place into offset of instruction
+public class Call : IInstruction {
+    private readonly int _label;
+    public Call(int label) {
+        _label = label & ~3;
+    }
+    public int Encode() {
+        return (0b0101 << 28) | _label;
+    }
+}
+
+// Return (OPTIONAL: deal with this in main!)
+// takes an offset, zeros out bottom bits, places it into the stack offset
+public class Return : IInstruction {
+    private readonly int _label;
+    public Return(int label) {
+        _label = label & ~3;
+    }
+    public int Encode() {
+        return (0b0110 << 28) | _label;
+    }
+}
+
+// Unary If
+// This will require translation in main! Expecting ints instead of strings.
+// Expects a 1001, 0, condition, PC relative
+public class UnaryIf : IInstruction {
+    private readonly int _cond;
+    private readonly int _label;
+    public UnaryIf(int cond, int label) {
+        _cond = cond;
+        _label = label;
+    }
+    public int Encode() {
+        return (0b1001 << 28) | (0b0 << 27) | (_cond << 25) | _label;
+    }
+}
+
+// Binary If
+// if <cond> <label>
+// Takes 0b1000, condition, PC relative offset (label)
+public class BinaryIf : IInstruction {
+    private readonly int _cond;
+    private readonly int _label;
+    public BinaryIf(int cond, int label) {
+        _cond = cond;
+        _label = label;
+    }
+    public int Encode() {
+        return (0b1000 << 28) | (_cond << 25) | _label;
     }
 }
--- Main.cs
+++ Main.cs
@@ -28,10 +28,10 @@ class Assembler
 
         // open asm file and set up StreamReader
         StreamReader sr;
-        string fileName = args[0]?.Trim() ?? string.Empty;
+        string inputFileName = args[0]?.Trim() ?? string.Empty;
 
         try {
-            sr = new StreamReader(fileName);
+            sr = new StreamReader(inputFileName);
         }
         catch (Exception e) {
             Console.WriteLine(e.Message);
@@ -59,10 +59,142 @@ class Assembler
             }
 
             _instructionList.Add(line);
-            Console.WriteLine(line);
+            //Console.WriteLine(line);
 
+            // update counters
             programCounter += 4;
             lineCounter++;
+        }
+
+        // DEBUG: print instructions
+        // foreach (var v in _instructionList) {
+        //     Console.WriteLine(v);
+        // }
+
+        foreach (var s in _instructionList) {
+            string[] inst = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string instName = inst[0].ToLower();
+
+            IInstruction instruction = null;
+            switch (instName.ToLower()) {
+                case "exit":
+                    instruction = new Exit();
+                    break;
+                case "swap":
+                    instruction = new Swap();
+                    break;
+                case "nop":
+                    instruction = new Nop();
+                    break;
+                case "input":
+                    instruction = new Input();
+                    break;
+                case "stinput":
+                    if (inst.Length == 2 && int.TryParse(inst[1], out int value)) {
+                        instruction = new Stinput(value);
+                        break;
+                    } else {
+                        instruction = new Stinput(0x00FF_FFFF);
+                        break;
+                    }
+                case "debug":
+                    instruction = new Debug();
+                    break;
+                case "pop":
+                    instruction = new Pop();
+                    break;
+                case "add":
+                    instruction = new Add();
+                    break;
+                case "sub":
+                    instruction = new Sub();
+                    break;
+                case "mul":
+                    instruction = new Mul();
+                    break;
+                case "div":
+                    instruction = new Div();
+                    break;
+                case "rem":
+                    instruction = new Rem();
+                    break;
+                case "and":
+                    instruction = new And();
+                    break;
+                case "or":
+                    instruction = new Or();
+                    break;
+                case "xor":
+                    instruction = new Xor();
+                    break;
+                case "lsl":
+                    instruction = new Lsl();
+                    break;
+                case "lsr":
+                    instruction = new Lsr();
+                    break;
+                case "asr":
+                    instruction = new Asr();
+                    break;
+                case "neg":
+                    instruction = new Neg();
+                    break;
+                case "not":
+                    instruction = new Not();
+                    break;
+                case "stprint":
+                    instruction = new Stprint();
+                    break;
+                // case "call":
+                //     instruction = new Call();
+                //     break;
+                // case "return":
+                //     instruction = new Return();
+                //     break;
+                // case "goto":
+                //     instruction = new Goto();
+                //     break;
+                // needs all forms and unary and binary!
+                // case "if":
+                //     instruction = new If();
+                //     break;
+                // case "dup":
+                //     instruction = new Dup();
+                //     break;
+                case "print":
+                    instruction = new Print();
+                    break;
+                case "dump":
+                    instruction = new Dump();
+                    break;
+                // case "push":
+                //     instruction = new Push();
+                //     break;
+            }
+
+            if (instruction != null) {
+                instructionList.Add(instruction);
+            }
+        }
+
+        // DEBUG: print interfaces
+        // foreach (var v in instructionList) {
+        //     Console.WriteLine(v);
+        // }
+
+        FileStream fs;
+        string outputFileName = args[1]?.Trim() ?? string.Empty;
+
+        fs = new FileStream(outputFileName, FileMode.Create);
+        BinaryWriter bWriter = new BinaryWriter(fs);
+
+        bWriter.Write((byte)0xDE);
+        bWriter.Write((byte)0xAD);
+        bWriter.Write((byte)0xBE);
+        bWriter.Write((byte)0xEF);
+
+        foreach (var inst in instructionList) {
+            bWriter.Write(inst.Encode());
         }
     }
 }
